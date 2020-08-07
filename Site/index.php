efe10955ca17045b8636aa09adf9a28524c5e469
Regier Kunkel <regier.kunkel@gmail.com>
2020-08-07 04:00:00 +0100

    Making use of the footer and header.

--- Site/index.php
+++ Site/index.php
@@ -1,12 +1,5 @@
- <!DOCTYPE html>
-<html>
-<body>
-
 <?php
-<?php include 'header.php' ?>
-<?php include 'footer.php' ?>
+include 'header.php'
+include 'footer.php'
 echo "Start. At least it's something.";
 ?>
-
-</body>
-</html> 
